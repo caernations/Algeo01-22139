477cfc5160d875305d1427e567332605ddfa42c0
stargeaze <yasminfsalma@gmail.com>
2023-09-23 18:23:23 +0700

    commit nyoba

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         double[][] matriks;
+        System.out.println("hello");
         
         matriks = ReadMatrices.Keyboard();
     }
