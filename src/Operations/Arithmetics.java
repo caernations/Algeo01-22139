41afbbc00aea5e9ead8c33250ec33cc53fa7a586
stargeaze <yasminfsalma@gmail.com>
2023-09-24 08:51:16 +0700

    update Arithmetics

--- src/Operations/Arithmetics.java
+++ src/Operations/Arithmetics.java
@@ -21,17 +21,17 @@ public class Arithmetics {
     }
 
     public static double[][] subtraction(double[][] matrix1, double[][] matrix2) {
-        int rows = matrix1.length;
-        int cols = matrix1[0].length;
+        int baris = matrix1.length;
+        int kolom = matrix1[0].length;
 
-        if (matrix2.length != rows || matrix2[0].length != cols) {
+        if (matrix2.length != baris || matrix2[0].length != kolom) {
             throw new IllegalArgumentException("Matriks harus memiliki jumlah baris dan kolom yang sama untuk pengurangan.");
         }
 
-        double[][] result = new double[rows][cols];
+        double[][] result = new double[baris][kolom];
 
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
+        for (int i = 0; i < baris; i++) {
+            for (int j = 0; j < kolom; j++) {
                 result[i][j] = matrix1[i][j] - matrix2[i][j];
             }
         }
