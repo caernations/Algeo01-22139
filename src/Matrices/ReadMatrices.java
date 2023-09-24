50fb084e8793876a5ddfe266de31bc180ccb7779
stargeaze <yasminfsalma@gmail.com>
2023-09-24 16:01:28 +0700

    fix bugs & update ReadMatrices.java

--- src/Matrices/ReadMatrices.java
+++ src/Matrices/ReadMatrices.java
@@ -4,28 +4,21 @@ import java.util.Scanner;
 public class ReadMatrices {
     public static double[][] Keyboard() {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Masukkan ukuran n (misalnya 3 untuk matriks 3x3): ");
+        
+        System.out.print("Masukkan jumlah baris (m): ");
+        int m = scanner.nextInt();
+        System.out.print("Masukkan jumlah kolom (n): ");
         int n = scanner.nextInt();
 
-        double[][] matrix = new double[n][n];
+        double[][] matrix = new double[m][n];
 
-        System.out.println("Masukkan matriks berukuran " + n + "x" + n + " dengan elemen-elemen dipisahkan oleh spasi dan baris-baris dipisahkan oleh newline:");
-
-        for (int i = 0; i < n; i++) {
+        System.out.println("Masukkan elemen-elemen matriks (pisahkan dengan spasi dan newline setelah setiap baris):");
+        for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (scanner.hasNext()) {
-                    String token = scanner.next();
-                    try {
-                        matrix[i][j] = Double.parseDouble(token);
-                    } catch (NumberFormatException e) {
-                        System.err.println("Input is not a valid double: " + token);
-                        return null;
-                    }
-                }
+                matrix[i][j] = scanner.nextDouble();
             }
         }
-        scanner.close();
+
         return matrix;
     }
 }
