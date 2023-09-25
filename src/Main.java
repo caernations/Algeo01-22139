16ffb4dfd18027345df4f26911255a4ce88dadb8
stargeaze <yasminfsalma@gmail.com>
2023-09-25 09:00:37 +0700

    update Main

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 import Matrices.*;
 import Operations.*;
 
@@ -14,11 +15,31 @@ public class Main {
         PrintMatrices.Print(matrix2);
 
         double[][] resultAddition = Arithmetics.Addition(matrix1, matrix2);
-        System.out.println("Hasil penjumlahan matriks:");
+        System.out.println("Hasil penjumlahan matriks: ");
         PrintMatrices.Print(resultAddition);
 
         double[][] resultSubtraction = Arithmetics.Subtraction(matrix1, matrix2);
-        System.out.println("Hasil pengurangan matriks:");
+        System.out.println("Hasil pengurangan matriks: ");
         PrintMatrices.Print(resultSubtraction);
+
+        double[][] resultMultiplyMatrix = Multiplies.MultiplyMatrix(matrix1, matrix2);
+        System.out.println("Hasil perkalian matriks: ");
+        PrintMatrices.Print(resultMultiplyMatrix);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan constant untuk perkalian dengan matriks: ");
+        double constant = scanner.nextDouble();
+        double[][] resultMultiplyByConst = Multiplies.MultiplyMatrixByConstant(matrix1, constant);
+        System.out.printf("Hasil perkalian matriks dengan constant %f: ", constant);
+        PrintMatrices.Print(resultMultiplyByConst);
+
+        System.out.print("Masukkan nilai mod untuk perkalian matriks dengan modulo: ");
+        double mod = scanner.nextDouble();
+        double[][] resultMultiplyWithMod = Multiplies.MultiplyMatrixWithMod(matrix1, matrix2, mod);
+        System.out.printf("Hasil perkalian matriks dengan modulo %f: ", mod);
+        PrintMatrices.Print(resultMultiplyWithMod);
+
+        scanner.close();
     }
 }
