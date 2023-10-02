f96238db4560fe545469c1bc87aed5356eb40232
stargeaze <yasminfsalma@gmail.com>
2023-10-02 16:14:55 +0700

    update ReadMatrices.java

--- src/Matrices/ReadMatrices.java
+++ src/Matrices/ReadMatrices.java
@@ -5,14 +5,18 @@ public class ReadMatrices {
     public static double[][] Keyboard() {
         Scanner scanner = new Scanner(System.in);
         
-        System.out.print("Masukkan jumlah baris (m): ");
+        System.out.print(">> Jumlah baris\t: ");
         int m = scanner.nextInt();
-        System.out.print("Masukkan jumlah kolom (n): ");
+        System.out.print(">> Jumlah kolom\t: ");
         int n = scanner.nextInt();
 
         double[][] matrix = new double[m][n];
+        System.out.println("""
 
-        System.out.println("Masukkan elemen-elemen matriks (pisahkan dengan spasi dan newline setelah setiap baris):");
+            Elemen-elemen matriks
+            ──────────────────────""");
+        System.out.println("> Gunakan spasi untuk pemisah antarkolom");
+        System.out.println("> Gunakan newline (enter) untuk pemisah antarbaris\n");
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 matrix[i][j] = scanner.nextDouble();
